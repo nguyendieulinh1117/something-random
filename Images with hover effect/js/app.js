c35835aaa810257db12734b46c06b1697e944788
Vicky Nguyen <ztieu.linh@gmail.com>
2021-07-07 14:56:16 +0700

    hover effect with GSAP

---
+++ Images with hover effect/js/app.js
@@ -0,0 +1,7 @@
+new hoverEffect({
+    parent: document.querySelector('.distortion'),
+    intensity: 0.3,
+    image1: './images/ice2.jpg',
+    image2: './images/ice.jpg',
+    displacementImage: './images/diss.png'
+})
